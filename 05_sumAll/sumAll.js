2e08cd01086995549a15a9924583945156dadf10
Drakon0807 <domideutsch1@web.de>
2024-11-08 12:44:03 +0100

    Edit and passed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if ((!Number.isInteger(num1) || num1 < 0) || (!Number.isInteger(num2) || num2 < 0)) return "ERROR";
 
+    let sum = 0;
+    if (num1 > num2) {
+        let x = num1;
+        num1 = num2;
+        num2 = x;
+
+    }
+    
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
