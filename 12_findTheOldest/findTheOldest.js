418adce17b81aa46b1d36ef8d0fadc1cad9f7d56
Drakon0807 <domideutsch1@web.de>
2024-11-12 13:52:42 +0100

    Edit and passed findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function age(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function (arr) {
+    return arr.reduce((oldest, person) => {
+        const oldestAge = age(oldest.yearOfDeath, oldest.yearOfBirth);
+        const personAge = age(person.yearOfDeath, person.yearOfBirth);
+
+        return oldestAge > personAge ? oldest : person;
+    });
 };
 
 // Do not edit below this line
