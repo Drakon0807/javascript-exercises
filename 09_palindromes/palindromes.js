7c1954e5fb0dadf4e1ebe72a959cfc109b9ea03b
Drakon0807 <domideutsch1@web.de>
2024-11-12 13:11:28 +0100

    Edit and passed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedString = str.toLowerCase().split("").filter(char => char.match("[\\w\\d]")).join("");
+    const reversedString = cleanedString.split("").reverse().join("");
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
